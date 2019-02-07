671eb16855aba0bc5b136a455fbf5bd73f875bac
Sandeep Pudi <40191692+spudi84@users.noreply.github.com>
2019-02-07 02:58:11 -0800

    Final Iteration 02072019

--- Sort_csv.py
+++ Sort_csv.py
@@ -1,64 +1,48 @@
 import csv
+import logging
 
-# input_file = 'Input_null.csv'
 input_file = 'input.csv'
 output_file = 'output.csv'
 
-# input_data = open(input_file, 'r')
-#
-# dialect = csv.Sniffer().sniff(input_data.read(1024))
-#
-# print (dialect)
-#
-# for row in input_data:
-#     # unicode_row = row.("utf-8")
-#     row = row.strip('\n')
-#     values = row.split(',')
-#     # unicode_values = values.decode("utf-8")
-#     # values = values.strip('\n')
-#     # Ignoring the case before sorting
-#     values.sort(key=lambda v: v.upper(), reverse=True)
-#     print(values)
-
-# TODO: Make the script unicode friendly utf-8, utf-8-sig, utf-16 etc.
-
-def read_n_sort(input_file, null_ind):
-    row = None
-    if null_ind:
-        return row
-    else:
-        with open(input_file, 'r', newline='', encoding='utf-8-sig') as csvfile:
-            # Check if the file is empty
-            # contents = csvfile.read()
-            # row = None
-            # if contents:
-            input_data = csv.reader(csvfile, delimiter=',')
-            for row in input_data:
+def read_n_sort(input_file):
+    with open(input_file, 'r', newline='', encoding='utf-8-sig') as csvfile:
+        # , encoding='utf-8-sig'
+        input_data = csv.reader(csvfile, delimiter=',')
+        # Initialize output as null. None doesn't work with writerow function
+        output_data = ''
+        # Check if file is empty
+        if input_data:
+            for output_data in input_data:
                 # Ignoring the case before sorting
-                row.sort(key=lambda v: v.upper(), reverse=True)
-                # row.sort(reverse=True)
-                print (row)
-            return row
+                output_data.sort(key=lambda v: v.upper(), reverse=True)
+                # Should only sort a single line
+                logging.info('First line sort complete')
+                break
+        else:
+            logging.info('Empty file or no data')
+        return output_data
 
 def write_file(row):
-    with open(output_file, 'w', newline='', encoding='utf-8-sig') as out:
+    with open(output_file, 'w') as out:
         csvwriter = csv.writer(out)
-        if row:
-            csvwriter.writerow(row)
-        else:
-            csvwriter.writerow('')
+        csvwriter.writerow(row)
 
-def empty_file_check(input_file):
-    with open(input_file, 'r') as ifile:
-        contents = ifile.read()
-        null_ind = True
-        if contents:
-            null_ind = False
-        return null_ind
-
+# Main function that drives the program
 def main():
-    null_ind = empty_file_check(input_file)
-    sorted_line = read_n_sort(input_file, null_ind)
-    write_file(sorted_line)
-
+    logging.basicConfig(filename='Sort_csv.log', filemode='w', level=logging.INFO)
+    try:
+        logging.info('Start to read and sort the input file')
+        # Read and Sort the input.csv
+        sorted_line = read_n_sort(input_file)
+        logging.info('Sort complete. Write to output started')
+        # Write the sorted csv line to output.csv
+        write_file(sorted_line)
+        logging.info('Write to output completed')
+    # Input file not found or has a different name or extension
+    except FileNotFoundError:
+        logging.info('File not found. Check the file availability, file''s extension or filename')
+        write_file('')
+    except MemoryError:
+        logging.info('Sort function out of memory')
+        write_file('')
 main()
